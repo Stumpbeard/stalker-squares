213562a0a5f2c6e9c19f453f82a7b15b5ec79017
Zak Thompson <zachary.thompson@nomadhealth.com>
2021-04-22 10:11:46 -0400

    Add pwa nonsense

---
+++ sw.js
@@ -0,0 +1,69 @@
+const cacheName = "bbb-v1";
+const contentToCache = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/engine.js",
+  "/sw.js",
+  "/font.png",
+  "/guy.jpeg",
+  "/guy.png",
+  "/icon.png",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/manifest.json",
+  "/spritesheet.png",
+  "/style.css",
+  "/sound/combo.wav",
+  "/sound/confirm.wav",
+  "/sound/enemy.wav",
+  "/sound/lose.wav",
+  "/sound/lose2.wav",
+  "/sound/select.wav",
+  "/sound/switch.wav",
+  "/sound/win.wav",
+];
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./pwa-examples/js13kpwa/sw.js");
+}
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      await cache.addAll(contentToCache);
+    })()
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      Promise.all(
+        keyList.map((key) => {
+          if (key === cacheName) {
+            return;
+          }
+          caches.delete(key);
+        })
+      );
+    })()
+  );
+});
